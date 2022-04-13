cbfc4b12f1f776ae852c3f3c898ef85d2fd198a0
Rahul Shiwarkar <rahuldnyan1242@gmail.com>
2022-04-13 14:28:27 +0530

    Remove FormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,26 +4,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './includes/header/header.component';
-import { PromiseComponent } from './promise/promise.component';
-import { ObservableComponent } from './observable/observable.component';
-import { FromEventComponent } from './observable/from-event/from-event.component';
-import { ListComponent } from './observable/list/list.component';
-import { IntervalComponent } from './observable/interval/interval.component';
 import {MatDividerModule} from '@angular/material/divider';
-import { OfFromComponent } from './observable/of-from/of-from.component';
-import { ToArrayComponent } from './observable/to-array/to-array.component';
-import { CustomComponent } from './observable/custom/custom.component';
 import {MatCardModule} from '@angular/material/card';
-import { TestingComponent } from './observable/testing/testing.component';
-import { MapComponent } from './observable/map/map.component';
-import { PluckComponent } from './observable/pluck/pluck.component';
-import { FilterComponent } from './observable/filter/filter.component';
-import { TapComponent } from './observable/tap/tap.component';
-import { TakeComponent } from './observable/take/take.component';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { PracticeModule } from './practice/practice.module';
 import { ObservableModule } from './observable/observable.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -38,7 +24,6 @@ import { ObservableModule } from './observable/observable.module';
     MatDividerModule,
     MatCardModule,
     HttpClientModule,
-    FormsModule,
     ReactiveFormsModule,
     PracticeModule,
     ObservableModule
